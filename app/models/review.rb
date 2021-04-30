79e378ec37c5dfad9c077b0a737e09622eca7107
Lydia <lreitzel@gmail.com>
2021-04-30 01:27:52 -0400

    removed meal variables and routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ActiveRecord::Base
     belongs_to :user
-    belongs_to :meal
     validates :title, presence: true
     validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
     has_many :reviews
-    has_many :meals, through: :reviews
     has_secure_password
     validates :username, presence: true
     validates :password, presence: true
