ba7d65590a7f82ea92e348b5fd2e0cf4ebd0122c
Lydia <lreitzel@gmail.com>
2021-04-30 01:25:47 -0400

    added title repitition prevention

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -14,41 +14,39 @@ class ReviewController < ApplicationController
     end
 
     post '/reviews' do
-        @review = Review.new(params)
-        wrong_user?
-        if @review.title.blank? || @review.body.blank?
-            erb :'/reviews/new'
-        else
+        @review = Review.new(params[:review])
+        if logged_in? && !Review.find_by(title: params[:title])
             @review.user = current_user
             @review.user_id = session[:user_id]
-            @review.meal_id = params[:review][:meal_id]
             current_user.reviews << @review
             @review.save
             erb :"/reviews/index"
+        else
+            erb :"/reviews/new"
         end
     end
 
     get '/reviews/:id' do
+        @review = Review.find(params[:id])
         wrong_user?
-        @review = Review.find(params["id"])
         erb :'/reviews/show'
     end
 
     get '/reviews/:id/edit' do
-        @review = Review.find(params["id"])
+        @review = Review.find(params[:id])
         wrong_user?
         erb :'/reviews/edit'
     end
 
     patch '/reviews/:id' do
-        @review = Review.find(params["id"])
+        @review = Review.find(params[:id])
         wrong_user?
         @review.update(params["review"])
         redirect to "/reviews/#{@review.id}"
     end
 
     delete '/reviews/:id' do
-        @review = Review.find(params["id"])
+        @review = Review.find(params[:id])
         wrong_user?
         @review.destroy
         redirect to '/reviews'
